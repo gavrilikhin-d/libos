9a44912daabc36f6bbf2fcf7dfde2d2245f04212
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2021-09-08 20:18:04 +0300

    Add source parsing

--- header-only.py
+++ header-only.py
@@ -9,17 +9,25 @@ from shutil import copy
 
 import re
 
-# Get content of a header file without license and header guard
-def header_content(path: str, license_end_line: int = 29) -> str:
-    with open(path) as h:
-        lines = h.readlines()
+# Remove #include "..." lines
+def lines_without_user_includes(path: str) -> list[str]:
+    with open(path) as f:
+        lines = f.readlines()
         remove_indexes = []
         for (i, line) in enumerate(lines):
             if line.startswith("#include \""):
                 remove_indexes.append(i)
         for i in reversed(remove_indexes):
             lines.pop(i)
-        return ''.join(lines[license_end_line + 1:])
+    return lines
+
+# Get content of a header file without license and header guard
+def header_content(path: str, license_end_line: int = 29) -> str:
+    return ''.join(lines_without_user_includes(path)[license_end_line + 1:])
+
+# Get content of a source file without #include "..."
+def source_content(path: str) -> str:
+    return ''.join(lines_without_user_includes(path))
 
 for filename in filenames:
     if filename in ignore: continue
@@ -40,10 +48,12 @@ for filename in filenames:
                 lines[i + 1] = f" *  {description}\n"
                 break
 
+        dependencies = [filename]
         # Substitute includes
         for (i, line) in enumerate(lines):
             if line.startswith("#include \""):
                 dependency = re.match("#include \"(.*)\"", line).group(1)
+                dependencies.append(dependency.removeprefix("os/"))
                 content = header_content("include/" + dependency)
                 lines[i] = \
                 f"// #include \"{dependency}\"\n" \
@@ -52,6 +62,44 @@ for filename in filenames:
                 f"// End of   \"{dependency}\"\n" \
                  "// =========================\n" \
                  "\n"
+
+        lines.append(
+            "\n"                             \
+            "\n"                             \
+            "// -------------------------\n" \
+            "// |        SOURCES        |\n" \
+            "// -------------------------\n" \
+            "\n"
+        )
+
+        for dependency in dependencies:
+            if dependency in ignore or dependency == "os.hpp": continue
+
+            src = dependency.removesuffix("pp").removesuffix("h") + "cpp"
+            linux_path = "src/linux/" + src
+            linux_content = source_content(linux_path)
+            windows_path = "src/windows/" + src
+            windows_content = source_content(windows_path)
+            lines.append(
+                 "#if IS_OS_LINUX\n"              \
+                f"// {linux_path}\n"              \
+                 "// =========================\n" \
+                f"{linux_content}\n"              \
+                f"// End of {linux_path}\n"       \
+                 "// =========================\n" \
+                 "\n"                             \
+                 "#endif // IS_OS_LINUX\n"        \
+                 "\n"                             \
+                 "#if IS_OS_WINDOWS\n"            \
+                f"// {windows_path}\n"            \
+                 "// =========================\n" \
+                f"{windows_content}\n"            \
+                f"// End of {windows_path}\n"     \
+                 "// =========================\n" \
+                 "\n"                             \
+                 "#endif // IS_OS_WINDOWS\n"
+            )
+
 
         f.seek(0)
         f.truncate()
